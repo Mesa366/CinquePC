18156df78ef6377fab8641504cd2095ce286017a
kawaK <67514738+kawaK1320@users.noreply.github.com>
2022-01-20 20:07:27 -0300

    inicializador
    DALE QUE VAAAAAA

---
+++ src/main/java/com/cinque/pc/CinquePcApplication.java
@@ -0,0 +1,13 @@
+package com.cinque.pc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinquePcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinquePcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinque/pc/CinquePcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinque.pc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinquePcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
