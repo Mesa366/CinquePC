0982d19204af139b79d47e4df754dd114e5e4e21
Franco Lamberti <franco_lamberti_02@yahoo.com.ar>
2022-01-20 19:24:28 -0300

    First commit

---
+++ src/test/java/com/cinquepc/cinquepc/CinquepcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinquepc.cinquepc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinquepcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
