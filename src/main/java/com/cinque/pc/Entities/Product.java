3b4733e06634378bfa7976d15bf869a8954eed98
David <davidgonzalezsantiago65@gmail.com>
2022-01-24 19:39:16 -0300

    Finished product entity and product repository

--- src/main/java/com/cinque/pc/Entities/Product.java
+++ src/main/java/com/cinque/pc/Entities/Product.java
@@ -1,7 +1,130 @@
 package com.cinque.pc.Entities;
 
-public class Product {
+import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
 	
+	private String id;
+
+	private String name;
+
+	private Double price;
+
+	private MyUser seller;
+
+	private MyUser buyer;
+	@Temporal(TemporalType.DATE)
 	
+	private Date sellingDate;
+	@Temporal(TemporalType.DATE)
+	
+	private Date buyingDate;
+
+	private List<String> categories;
+
+	private Integer stock;
+
+	public Product(String id, String name, Double price, MyUser seller, Date sellingDate, Date buyingDate,
+			List<String> categories, Integer stock, MyUser buyer) {
+
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.seller = seller;
+		this.sellingDate = sellingDate;
+		this.buyingDate = buyingDate;
+		this.categories = categories;
+		this.stock = stock;
+		this.buyer = buyer;
+	}
+
+	public Product() {
+
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public MyUser getSeller() {
+		return seller;
+	}
+
+	public void setSeller(MyUser seller) {
+		this.seller = seller;
+	}
+
+	public MyUser getBuyer() {
+		return buyer;
+	}
+
+	public void setBuyer(MyUser buyer) {
+		this.buyer = buyer;
+	}
+
+	public Date getSellingDate() {
+		return sellingDate;
+	}
+
+	public void setSellingDate(Date sellingDate) {
+		this.sellingDate = sellingDate;
+	}
+
+	public Date getBuyingDate() {
+		return buyingDate;
+	}
+
+	public void setBuyingDate(Date buyingDate) {
+		this.buyingDate = buyingDate;
+	}
+
+	public List<String> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<String> categories) {
+		this.categories = categories;
+	}
+
+	public Integer getStock() {
+		return stock;
+	}
+
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
 }
