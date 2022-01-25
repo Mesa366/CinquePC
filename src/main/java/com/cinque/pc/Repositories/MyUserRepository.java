e65ae77139dbb715f3434f6a92e4a08c9fbb9546
Alan Coradi <alanncoradi@gmail.com>
2022-01-25 20:13:31 -0300

    MyUserRepo findByEmail

--- src/main/java/com/cinque/pc/Repositories/MyUserRepository.java
+++ src/main/java/com/cinque/pc/Repositories/MyUserRepository.java
@@ -1,11 +1,18 @@
 package com.cinque.pc.Repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.cinque.pc.Entities.MyUser;
 
 @Repository
 public interface MyUserRepository extends JpaRepository<MyUser, String>{
+	
+	@Query("SELECT a FROM MyUser a WHERE a.email= :email")
+	Optional<MyUser> getByEmail(@Param("email") String email);
 	
 }
