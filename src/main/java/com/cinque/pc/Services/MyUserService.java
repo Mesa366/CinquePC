64676c0d5469beb3b59639a0d1c74169fc4c6a17
AnaMurinigo <anamurinigo@gmail.com>
2022-01-31 19:51:46 -0300

    Fix in MyUserService

--- src/main/java/com/cinque/pc/Services/MyUserService.java
+++ src/main/java/com/cinque/pc/Services/MyUserService.java
@@ -41,9 +41,10 @@ public class MyUserService implements UserDetailsService{
 
 	@Autowired
 	private MyUserRepository userRepo;
-	@Autowired
+	
+	/*@Autowired
 	private MyUser user;
-
+*/
 	//CREATE
 	/**
 	 * Creates user 
