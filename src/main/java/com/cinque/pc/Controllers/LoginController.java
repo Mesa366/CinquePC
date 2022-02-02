d1f9e0f771fe55658a213ea5431b4fd90955d226
David <davidgonzalezsantiago65@gmail.com>
2022-02-02 19:08:47 -0300

    Create LoginController.java

---
+++ src/main/java/com/cinque/pc/Controllers/LoginController.java
@@ -0,0 +1,28 @@
+package com.cinque.pc.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class LoginController {
+
+	
+	@GetMapping("/form/{state}")
+	public String form(@PathVariable String state, Model model) {
+		if(!(state.equals("register")) || !(state.equals("login"))) {
+			return "redirect:../";
+		}
+		if (state.equals("login")) {
+			model.addAttribute("state", "login");
+		}
+		if (state.equals("register")) {
+			model.addAttribute("state", "register");
+		}
+		return"formUser";
+	}
+
+}
