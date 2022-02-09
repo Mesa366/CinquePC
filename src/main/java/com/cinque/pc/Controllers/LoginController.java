6f9dec5695f5db8132991ea2ea3997c28c61a275
daianaghisio <daianaghisio@gmail.com>
2022-02-09 19:45:16 -0300

    modified form thymeleaf and myUser controller

--- src/main/java/com/cinque/pc/Controllers/LoginController.java
+++ src/main/java/com/cinque/pc/Controllers/LoginController.java
@@ -22,36 +22,41 @@ public class LoginController {
 	@Autowired
 	MyUserService myUserService;
 
-	
-	@GetMapping("/form/{state}/{id}")
-	public String form(@PathVariable("state") String state, Model model, @PathVariable("id") String id) {
-		
-	System.out.println("id: "+id);
-		
-		
-		if(!(state.equals("register")) && !(state.equals("login")) && !(state.equals("update"))) {
-			return "redirect:../";
-		}
+	@GetMapping("/form/{state}")
+	public String showForm(@PathVariable("state") String state, Model model) {
+
 		if (state.equals("login")) {
 			model.addAttribute("state", "login");
-		}
-		if (state.equals("register")) {
+		} else if (state.equals("register")) {
 			model.addAttribute("state", "register");
-			
 			model.addAttribute("myUser", new MyUser());
-			
+
+		} else {
+			return "redirect:../";
 		}
-		if (state.equals("update")) {
-			model.addAttribute("state", "update");
-			
-			model.addAttribute("myUser", myUserService.getById(id));			
-			
-			
-		}		
-		return"layout/user";
+
+		return "layout/user";
+
 	}
 
-	//TODO TEST (ESTE FUNCA!)
+	@GetMapping("/form/{state}/{id}")
+	public String form(@PathVariable("state") String state, Model model, @PathVariable("id") String id) {
+
+		/* TODO cambiar type por password en el html user */
+
+		if ((state.equals("delete"))) {
+			myUserService.deleteUser(id);
+			return "redirect:../";
+		} else if (state.equals("update")) {
+			model.addAttribute("state", "update");
+			model.addAttribute("myUser", myUserService.getById(id));
+		} else {
+			return "redirect:../";
+		}
+		return "layout/user";
+	}
+
+	// TODO TEST (ESTE FUNCA!)
 //	@GetMapping("/form/{id}")
 //	public String form1(Model model, @PathVariable("id") String id) {
 //		
@@ -61,15 +66,13 @@ public class LoginController {
 //		
 //		return"layout/user";
 //	}
-	
-	
-	
-	/**
-	USER UPDATE
-	*/
 
-	//TODO method for changing password securely
-	//TODO can the user simply change his/her email?
+	/**
+	 * USER UPDATE
+	 */
+
+	// TODO method for changing password securely
+	// TODO can the user simply change his/her email?
 //	@PostMapping("/update/{id}")
 //	public String updateUserRedirect(@PathVariable String id,Image profilePicture,
 //	String name, String password, String email, Integer dni, Integer phone,
@@ -80,43 +83,36 @@ public class LoginController {
 //		
 //	}
 
-		
-	//USER REGISTER
+	// USER REGISTER
 	@PostMapping("/register")
-	public String registerUserRedirect(Image profilePicture,
-	String name, String password, String email, Integer dni, Integer phone) throws Exception {
-		
-		myUserService.createUser(name, password, email, dni, phone, null, null);		
-		
-		
-		
+	public String registerUserRedirect(Image profilePicture, String name, String password, String email, Integer dni,
+			Integer phone) throws Exception {
+
+		myUserService.createUser(name, password, email, dni, phone, null, null);
+
 		return "redirect:../";
-		
+
 	}
-	
-	//UPDATE REGISTER
+
+	// UPDATE REGISTER
 	@PostMapping("/update/{id}")
-	public String updateUserRedirect(@PathVariable String id, Image profilePicture,
-	String name, String password, String email, Integer dni, Integer phone) throws Exception {
-		
-		myUserService.updateUser(id, name, password, email, dni, phone, null, null);	
-		
-		
-		
+	public String updateUserRedirect(@PathVariable String id, Image profilePicture, String name, String password,
+			String email, Integer dni, Integer phone) throws Exception {
+
+		myUserService.updateUser(id, name, password, email, dni, phone, null, null);
+
 		return "redirect:../";
-		
+
 	}
-	
 
 	/*
 	 * @PostMapping("/register")
-	
-	public String registerUserRedirect(@RequestParam MyUser myUser) {
-		
-		myUserService.createUser(myUser);
-		
-		return "redirect:../";
-	}
+	 * 
+	 * public String registerUserRedirect(@RequestParam MyUser myUser) {
+	 * 
+	 * myUserService.createUser(myUser);
+	 * 
+	 * return "redirect:../"; }
 	 */
 
 }
